c9918a46f5ca1ceabcd871ac4ace7d5bf0f42d86
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-11 11:58:49 +0100

    working with functional interface

--- src/FunctionalInterface/ClientTest.java
+++ src/FunctionalInterface/ClientTest.java
@@ -9,8 +9,10 @@ public class ClientTest {
 	
 	private static void threadUsingLamdaExpression() {
 		/** Converting Anonymous Thread Method to Lambda expression */
-		/** Since functional interface have only one method, lamdbda */
+		/** Since functional interface have only one method, lambda expression can easily provide
+		 * the method implementation. */
 		Runnable r = () -> System.out.println("My function is excuting...");
+		
 		
 		Thread thread = new Thread(r);
 		thread.start();
