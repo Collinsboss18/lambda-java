c220965d31cbb341326a50103a4bfe17fbe04923
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-11 11:34:29 +0100

    working with functional interface

--- src/FunctionalInterface/ClientTest.java
+++ src/FunctionalInterface/ClientTest.java
@@ -3,6 +3,16 @@ package FunctionalInterface;
 public class ClientTest {
 
 	public static void main(String[] args) {
+		threadUsingAnonymous();
+		threadUsingLamdaExpression();
+	}
+
+	private static void threadUsingLamdaExpression() {
+		
+		
+	}
+
+	public static void threadUsingAnonymous() {
 		Runnable r = new Runnable() {
 			
 			public void run () {
